7e0492ef507931a7a7a80d587ba723b34c7cdcb4
seunghee <kanada11416@gmail.com>
2023-09-11 11:02:17 +0900

    :tada: init project

---
+++ quddy/src/main/java/com/team_quddy/quddy/QuddyApplication.java
@@ -0,0 +1,13 @@
+package com.team_quddy.quddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuddyApplication.class, args);
+	}
+
+}
---
+++ quddy/src/test/java/com/team_quddy/quddy/QuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team_quddy.quddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
