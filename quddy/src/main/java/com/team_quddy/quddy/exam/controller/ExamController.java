91b7c617a4c556f31c09d41ab6beb48044e7ca58
seunghee <kanada11416@gmail.com>
2023-09-12 16:05:54 +0900

    :art: modify how to receive a request of getTemplateDetail, getExam

--- quddy/src/main/java/com/team_quddy/quddy/exam/controller/ExamController.java
+++ quddy/src/main/java/com/team_quddy/quddy/exam/controller/ExamController.java
@@ -1,14 +1,12 @@
 package com.team_quddy.quddy.exam.controller;
 
+import com.team_quddy.quddy.exam.domain.request.ExamReq;
 import com.team_quddy.quddy.exam.domain.response.ExamRes;
 import com.team_quddy.quddy.exam.domain.response.ExamsRes;
 import com.team_quddy.quddy.exam.domain.response.TemplateDetailRes;
 import com.team_quddy.quddy.exam.service.ExamService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,13 +19,13 @@ public class ExamController {
         return new ExamsRes(examService.getPopularTemplate());
     }
 
-    @GetMapping("/template/detail/{id}")
-    public TemplateDetailRes getTemplateDetail(@PathVariable(value = "id") Integer id) {
-        return examService.getTemplateDetail(id);
+    @GetMapping("/template/detail")
+    public TemplateDetailRes getTemplateDetail(@RequestBody ExamReq examReq) {
+        return examService.getTemplateDetail(examReq.getId());
     }
 
-    @GetMapping("/solver/exam/{id}")
-    public ExamRes getExam(@PathVariable(value = "id") Integer id) {
-        return examService.getExam(id);
+    @GetMapping("/solver/exam")
+    public ExamRes getExam(@RequestBody ExamReq examReq) {
+        return examService.getExam(examReq.getId());
     }
 }
---
+++ quddy/src/main/java/com/team_quddy/quddy/exam/domain/request/ExamReq.java
@@ -0,0 +1,8 @@
+package com.team_quddy.quddy.exam.domain.request;
+
+import lombok.Getter;
+
+@Getter
+public class ExamReq {
+    private Integer id;
+}
